b9582ccde9566bd851719add07607c228f8fcb7b
Zhenghao Lou <rhzx3519@gmail.com>
2024-02-02 23:23:36 +1100

    feat: introduce nextui

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './app/store'
-import { Provider } from 'react-redux'
+// 1. import `NextUIProvider` component
+import {NextUIProvider} from "@nextui-org/react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <NextUIProvider>
+          <main className="light text-foreground bg-background">
+              <App />
+          </main>
+      </NextUIProvider>
   </React.StrictMode>
 );
 
